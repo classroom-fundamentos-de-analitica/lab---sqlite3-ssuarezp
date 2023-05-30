70ddd5711c8f0cffd9e61b9e6e68aee797c75d21
ssuarezp <125924812+ssuarezp@users.noreply.github.com>
2023-05-29 20:22:20 -0500

    20230529
    2

--- pregunta_11.sql
+++ pregunta_11.sql
@@ -37,6 +37,6 @@
 --  0  2018     6
 --
 --  >>> Escriba su codigo a partir de este punto <<<
-SELECT strftime('%Y',c14), COUNT(*) FROM tbl1 
+SELECT COUNT(*) FROM tbl1 
 WHERE strftime('%Y',c14) = '2018';
 -- 
